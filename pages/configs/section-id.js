8df4dcc24cdc154539d539952b4f5b32c2e6f625
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-09-25 17:13:42 -0300

    feat: configuring supported sections

---
+++ pages/configs/section-id.js
@@ -0,0 +1,9 @@
+/* Constante responsável por agrupar os IDs das editorias.
+ * Esses IDs foram retirados diretamente do CMS
+ */
+export const SECTION_ID = {
+  NOTICIAS: '4f70b9a96c4db25194000110',
+  ENTRETENIMENTO: '502510c61d420654b1000001',
+  ESPORTES: '4f99869f6c4db22c70000004',
+  LIFE_STYLE: '5d5c4952cd77c07e400003c4',
+}
